1522000ce447825a9a049379c42f2587475f5c25
Harshit Grover <codrrharshit@gmail.com>
2026-04-03 00:39:59 +0530

    db config change

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,17 @@
-const mongoose=require('mongoose');
-const connectDB= async()=>{
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("Connected to mongodb");
-    } catch (error) {
-        console.log("Mongodb connection failed", error.message);
-        throw error;
-    }
-}
+const mongoose = require('mongoose');
 
-module.exports=connectDB;
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('MongoDB connection failed', err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
 
